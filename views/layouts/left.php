9be325fd1ab00f1a1160f7541bfeca0cb26374e6
desh <serdesh77@mail.ru>
2021-03-22 14:38:33 +0300

    Изменен аватар пользователя

--- views/layouts/left.php
+++ views/layouts/left.php
@@ -11,7 +11,7 @@ use dmstr\widgets\Menu;
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
+                <img src="/img/user.png" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
                 <p><?= Yii::$app->user->identity->fio ?></p>
