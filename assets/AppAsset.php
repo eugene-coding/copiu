5486fa058149197201074e73705ec78f944c33b2
desh <serdesh77@mail.ru>
2021-04-19 16:45:59 +0300

    Добавлен файл стилей для верстки

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -22,6 +22,7 @@ class AppAsset extends AssetBundle
     public $css = [
         'css/site.css',
         'css/main.css',
+        'css/mobile.css',
     ];
     public $js = [
         'js/main.js',
