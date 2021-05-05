2a36bd1d851351f3b171475fae9a4381c1067e9f
desh <serdesh77@mail.ru>
2021-05-05 16:57:28 +0300

    fix

--- web/js/order_blank.js
+++ web/js/order_blank.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
                if (response.success){
                    message_block.html('<i class="glyphicon glyphicon-ok text-success"></i> '
                        + response.data);
-                   $('tbody').find('[data-col-seq="5"]').text(response.date)
+                   $('tbody').find('[data-col-seq="6"]').text(response.date)
                } else {
                    message_block.html('<i class="glyphicon glyphicon-alert text-danger"></i> ' + response.error).fadeIn(300);
                }
