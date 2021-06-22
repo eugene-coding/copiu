e630b268826019c778d3c362a7eb7f4c972cfe32
desh <serdesh77@mail.ru>
2021-06-22 15:50:35 +0300

    fix visual

--- views/order/_nomenclature.php
+++ views/order/_nomenclature.php
@@ -10,54 +10,59 @@ $counter = 1;
 
 ?>
 
-        <div class="table-products">
-            <table class="table table-bordered table-hover table-nomenclature">
-                <thead>
+<div class="table-products">
+    <table class="table table-bordered table-hover table-nomenclature">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Наименование</th>
+            <th>Описание</th>
+            <th>Количество</th>
+            <th>Ед. измерения</th>
+            <th>Цена</th>
+            <th>Итого</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php if ($dataProvider): ?>
+            <?php
+            /** @var array $product */
+            foreach ($dataProvider as $product): ?>
+                <?php
+                Yii::info($product, 'test');
+                if (!$product) {
+                    continue;
+                }
+                ?>
                 <tr>
-                    <th>#</th>
-                    <th>Наименование</th>
-                    <th>Описание</th>
-                    <th>Количество</th>
-                    <th>Ед. измерения</th>
-                    <th>Цена</th>
-                    <th>Итого</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php if ($dataProvider): ?>
-                    <?php
-                    /** @var array $product */
-                    foreach ($dataProvider as $product): ?>
+                <td aria-label="#"><?= $counter ?></td>
+                <td aria-label="Наименование"><?= $product['name'] ?></td>
+                <td aria-label="Описание">
+                    <div class="description">
                         <?php
-                        Yii::info($product, 'test');
-                        if (!$product) {
-                            continue;
-                        }
-
+                        echo $product['description'] ?: 'нет';
                         ?>
-                        <tr>
-                        <td aria-label="#"><?= $counter ?></td>
-                        <td aria-label="Наименование"><?= $product['name'] ?></td>
-                        <td aria-label="Описание"><?= $product['description'] ?: 'нет' ?></td>
-                        <td aria-label="Кол-во"><?= Html::input('number', "Order[count][{$product['obtn_id']}]",
-                                $product['count'],
-                                [
-                                    'data-obtn-id' => $product['obtn_id'],
-                                    'class' => 'form-control count-product',
-                                    'min' => 0,
-                                    'step' => 1,
-                                    'onkeypress' => 'return event.charCode >= 48'
-                                ]) ?></td>
-                        <td aria-label="Ед. изм."><?= $product['measure'] ?></td>
-                        <td aria-label="Цена" class="product-price"><?= $product['price'] ?></td>
-                        <td aria-label="Итого" class="total-cost"><?= $product['count'] * $product['price'] ?></td>
-                        <?php $counter++; ?>
-                    <?php endforeach; ?>
-                    </tr>
-                <?php else: ?>
-                    <p>Ничего не найдено</p>
-                <?php endif; ?>
-                </tbody>
-            </table>
-        </div>
+                    </div>
+                </td>
+                <td aria-label="Кол-во"><?= Html::input('number', "Order[count][{$product['obtn_id']}]",
+                        $product['count'],
+                        [
+                            'data-obtn-id' => $product['obtn_id'],
+                            'class' => 'form-control count-product',
+                            'min' => 0,
+                            'step' => 1,
+                            'onkeypress' => 'return event.charCode >= 48'
+                        ]) ?></td>
+                <td aria-label="Ед. изм."><?= $product['measure'] ?></td>
+                <td aria-label="Цена" class="product-price"><?= $product['price'] ?></td>
+                <td aria-label="Итого" class="total-cost"><?= $product['count'] * $product['price'] ?></td>
+                <?php $counter++; ?>
+            <?php endforeach; ?>
+            </tr>
+        <?php else: ?>
+            <p>Ничего не найдено</p>
+        <?php endif; ?>
+        </tbody>
+    </table>
+</div>
 
